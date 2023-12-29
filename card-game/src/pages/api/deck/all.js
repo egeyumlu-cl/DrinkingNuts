ad93501b241669e81e446b079591725cf934602b
Ege Yumlu <eeyumlu@gmail.com>
2023-12-29 14:43:28 +0100

    added the api for all

---
+++ card-game/src/pages/api/deck/all.js
@@ -0,0 +1,18 @@
+import prisma from "@/lib/prisma";
+
+export default async function handle(req, res) {
+  const { method, body } = req;
+  const code = 'aral4ever'
+  
+  switch (method) {
+    case "GET":
+        try {
+            const deck = await prisma.deck.findMany();
+
+            res.status(200).json(deck);
+        } catch (err) {
+            res.status(500).json({ error: err.message });
+        }
+    
+  }
+}
